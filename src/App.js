d9d58890e228ab8d5ce6296ee7b60d1369232594
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-08-19 03:28:37 +0300

    1.03

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import {React, useState} from 'react';
+
 import './App.css';
+import Post from './Post.js';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { 
+    username:"alexa", 
+    caption:"WOW it works", 
+    imageUrl:"https://sun9-47.userapi.com/N4NH_Qf8Gq5OrTPlecP-nKgz7CnvNkT8h1Wp9A/96NwACx41og.jpg"
+  },
+    {
+    username:"alexa", 
+    caption:"WOW it works", 
+    imageUrl:"https://sun9-47.userapi.com/N4NH_Qf8Gq5OrTPlecP-nKgz7CnvNkT8h1Wp9A/96NwACx41og.jpg"
+    }
+  ]);
+     
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <div className="app_header">
+        <img
+          className="app_headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="instagram"
+        />
+      </div>
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={imageUrl}/>
+        ))
+      }
+      <Post username="serg" caption="WOW it works"  imageUrl="https://sun9-73.userapi.com/Ae-JV-4ofQhVPe3DOvHy2Y7YdWD3GtwCsZNNRA/En8EMcFTOho.jpg"/>
+      <Post username="matv" caption="WOW it works" imageUrl="https://sun9-13.userapi.com/kO1M1bkb3e4Xv7tHerehOR4c_ue4uf4_MV0duw/HjAipjhxiDc.jpg" />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+
+import firebase from "firebase";
+
+  const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAnODTM3M8eTkgbE07wJzK-H-9JyeR3yN0",
+    authDomain: "insta-clone-react-84054.firebaseapp.com",
+    databaseURL: "https://insta-clone-react-84054.firebaseio.com",
+    projectId: "insta-clone-react-84054",
+    storageBucket: "insta-clone-react-84054.appspot.com",
+    messagingSenderId: "588424468429",
+    appId: "1:588424468429:web:57ed590ba8d10ccd08d684",
+    measurementId: "G-VERCEPYD17"
+  });
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const storage = firebase.storage();
+
+  
+  export default { db, auth, storage };
